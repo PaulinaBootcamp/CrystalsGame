77141ef04682b4a5f9ac53264140ed451eea5038
PaulinaBootcamp <paulinaoshafi@gmail.com>
2019-04-09 18:48:58 -0400

    Working, just needs reset

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,32 +7,55 @@ $("#number-to-guess").text(targetNumber);
 var counter = 0;
 wins = 0;
 losses = 0;
+counter = 0;
 document.getElementById("wins").innerHTML = (wins);
-document.getElementById("losses").innerHTML = (+ losses);
+document.getElementById("losses").innerHTML = (losses);
+document.getElementById("userGuessTotal").innerHTML = (counter);
 
 
 //assign unique number from 1-12 for each of the houses
 var gryffindor = Math.floor(Math.random() * (12 - 1 + 1)) + 1;;
-$("#gryffindor").text(gryffindor);
 var hufflepuff = Math.floor(Math.random() * (12 - 1 + 1)) + 1;;
 var ravenclaw = Math.floor(Math.random() * (12 - 1 + 1)) + 1;;
 var slytherin = Math.floor(Math.random() * (12 - 1 + 1)) + 1;;
 
 console.log(gryffindor, slytherin, hufflepuff, ravenclaw)
 
+$("#gryffindor").attr("data-number", gryffindor);
+$("#hufflepuff").attr("data-number", hufflepuff);
+$("#ravenclaw").attr("data-number", ravenclaw);
+$("#slytherin").attr("data-number", slytherin);
+
 $(".houseCrest").on("click", function () {
 
-    var clickValue = ($(this).id);
-    console.log(this.id);
+    var clickValue = ($(this).attr("data-number"));
+    clickValue = parseInt(clickValue);
+    console.log(clickValue);
     counter += clickValue;
+    console.log(counter);
+    document.getElementById("userGuessTotal").innerHTML = (counter);
+    console.log(counter);
+    console.log(targetNumber);
 
     if (counter === targetNumber) {
+
         wins++;
+        document.getElementById("wins").innerHTML = (wins);
+        document.getElementById("losses").innerHTML = (losses);
+        console.log(targetNumber)
+        console.log(counter)
     }
 
     else if (counter >= targetNumber) {
+
         losses++;
+        document.getElementById("wins").innerHTML = (wins);
+        document.getElementById("losses").innerHTML = (losses);
+        console.log(targetNumber)
+        console.log(counter)
     }
 
 });
+
+
 
